7e03448701ac0524a110613f323ae5a67eaa9a4c
sztyup <sztyup@gmail.com>
2019-03-28 14:31:39 +0100

    Remove unneeded code

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,16 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Entities\User;
-
 class IndexController extends Controller
 {
     public function index()
     {
-        $users = $this->em->getRepository(User::class)->findAll();
-
-        return $this->view('index', [
-            'users' => $users
-        ]);
+        return $this->view('index');
     }
 }
